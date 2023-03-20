668d2f3ce0788dfec8b1cf94998b85a27529349d
Michal <michalfica125@gmail.com>
2023-03-20 21:00:54 +0100

    big part done

--- traceroute/traceroute.cpp
+++ traceroute/traceroute.cpp
@@ -14,6 +14,8 @@
 #include <assert.h>
 #include <string>
 #include <iostream>
+#include <chrono>
+#include <ctime> 
 
 void print_as_bytes (unsigned char* buff, ssize_t length)
 {
@@ -97,6 +99,11 @@ int main( int argc, char *argv[])
 
             printf("ICMP packets send %ld\n", bytes_send);
         }
+        std::cout << "wysłałem wszystko dla ttl = " << ttl << '\n';
+        // auto start = std::chrono::system_clock::now();
+        // // Some computation here
+        // auto end = std::chrono::system_clock::now();
+        // std::chrono::duration<double> elapsed_seconds = end-start;
 
         // // wait for response packets ------------------------------------------------
         fd_set descriptors;
@@ -104,47 +111,87 @@ int main( int argc, char *argv[])
         FD_SET (sockfd, &descriptors);
 
         struct timeval tv; tv.tv_sec = 1; tv.tv_usec = 0;
-        int ready = select (sockfd+1, &descriptors, NULL, NULL, &tv);
-        if(ready < 0){
-            printf("select() error\n");
-        }
-        printf("gotowych pakietów do odebrania: %d\n", ready);
 
-        //---------------------------------------------------------------------------
-        // receive packets ----------------------------------------------------------
-        for(int j = 0; j < ready; j++){
+        int responses = 0;
+
+        while (responses < 3 && (tv.tv_sec > 0 || tv.tv_usec > 0))
+        {
+            std::cout<<"zaczynam czekać (max 1sec) na pojawienie się jakiegoś pakietu\n";
+            int ready = select (sockfd+1, &descriptors, NULL, NULL, &tv);
+            std::cout<<"gotowych pakietów jest: "<<ready<<'\n';
+
+            if(ready < 0){
+                printf("select() error\n");
+            } else if (ready == 0){
+                break;
+            }
+            
             struct sockaddr_in 	sender;	
             socklen_t 			sender_len = sizeof(sender);
             u_int8_t 			buffer[IP_MAXPACKET];
 
-            ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET, 0, (struct sockaddr*)&sender, &sender_len);
-            if (packet_len < 0) {
-                fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
-                return EXIT_FAILURE;
+            //---------------------------------------------------------------------------
+            // receive packets ----------------------------------------------------------
+            for (;;) {
+                std::cout<<"zaczynam odbieranie pakietów z gniazda receivem\n";
+
+                ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET, MSG_DONTWAIT, (struct sockaddr*)&sender, &sender_len);
+                std::cout<<"odebralem dl pakietu to: "<<packet_len<<'\n';
+
+                if (packet_len < 0) {
+                    fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
+                    // return EXIT_FAILURE;
+                    break;
+                }
+                if( packet_len == 0){
+                    break;
+                }
+
+                char sender_ip_str[20]; 
+                inet_ntop(AF_INET, &(sender.sin_addr), sender_ip_str, sizeof(sender_ip_str));
+                printf ("Received IP packet with ICMP content from: %s\n", sender_ip_str);
+
+                struct ip* 			ip_header = (struct ip*) buffer;
+                ssize_t				ip_header_len = 4 * ip_header->ip_hl;
+                u_int8_t* icmp_packet = buffer + 4 * ip_header->ip_hl;
+                struct icmp* icmp_header = (struct icmp*) icmp_packet;
+                ssize_t icmp_header_len = 8;
+                
+                // Type 8 - loopback
+                bool skip = false;
+                if(icmp_header->icmp_type == 8) {
+                    skip = true;
+                }
+
+                // Type 11 - TTL exceeded, extracting the original request
+                if(icmp_header->icmp_type == 11) {
+                    ssize_t inner_packet = ip_header_len + icmp_header_len; // Moving to inner ICMP
+                    ip_header = (struct ip *)(buffer + inner_packet);
+                    ip_header_len = 4 * ip_header->ip_hl;
+                    icmp_packet = buffer + inner_packet + ip_header_len;
+                    icmp_header = (struct icmp *)icmp_packet;
+                }
+
+                printf ("IP header: "); 
+                print_as_bytes (buffer, ip_header_len);
+                printf("\n");
+
+                printf ("IP data:   ");
+                print_as_bytes (buffer + ip_header_len, packet_len - ip_header_len);
+                printf("\n");
+
+                u_int16_t id  = icmp_header->icmp_hun.ih_idseq.icd_id;
+                u_int16_t seq = icmp_header->icmp_hun.ih_idseq.icd_seq;
+
+                std::cout << "id pakietu: " << id << " seq: " << seq << "\n\n";
+
+                // Checking if packet is a response to our last requests
+                if(seq == ttl && !skip) {
+                    responses++;
+                }
+
             }
-
-            char sender_ip_str[20]; 
-            inet_ntop(AF_INET, &(sender.sin_addr), sender_ip_str, sizeof(sender_ip_str));
-            printf ("Received IP packet with ICMP content from: %s\n", sender_ip_str);
-
-            struct ip* 			ip_header = (struct ip*) buffer;
-            ssize_t				ip_header_len = 4 * ip_header->ip_hl;
-            u_int8_t* icmp_packet = buffer + 4 * ip_header->ip_hl;
-            struct icmp* icmp_header = (struct icmp*) icmp_packet;
-
-            printf ("IP header: "); 
-            print_as_bytes (buffer, ip_header_len);
-            printf("\n");
-
-            printf ("IP data:   ");
-            print_as_bytes (buffer + ip_header_len, packet_len - ip_header_len);
-            printf("\n");
-
-            u_int16_t id  = icmp_header->icmp_hun.ih_idseq.icd_id;
-            u_int16_t seq = icmp_header->icmp_hun.ih_idseq.icd_seq;
-
-            std::cout << "id pakietu: " << id << " seq: " << seq << "\n\n";
-
+            
         }
     }
 }
